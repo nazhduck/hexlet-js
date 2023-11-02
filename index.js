ad92cfd0fe72b4be4a033be2dfa72978b954d161
Артём Тихонов <mishilen2002@gmail.com>
2023-11-02 09:30:46 +0300

    modified function in index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-import _ from 'lodash';
+import a from "./src/index.js";
 
-console.log(_.compact([1, 3, false]));
-
-console.log('Hello, Hexlet!');
-
-const a = (i) => {
-  console.log(i * 5);
-};
-
-a(4);
+a();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import _ from "lodash";
+
+export default () => {
+  console.log(_.compact([1, 3, false]));
+};
